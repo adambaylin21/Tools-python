c8c62e5f980ce04e865f859315050ded27ef382c
root <vip.phamquangthinh@gmail.com>
2018-01-11 20:32:06 +0700

    Python Function

--- Test.py
+++ Test.py
@@ -1,3 +1,15 @@
-key = {'a':'1','b':'2'}
-for ke in key:
-    print (key[ke])
+import random
+numList = []
+for i in range(100):
+    numList.append(random.randint(1,1000))
+
+print(numList)
+for i in range(1,len(numList)):
+    j = i
+    while j > 0:
+        if numList[j] < numList[j - 1]:
+            temp = numList[j-1]
+            numList[j-1] = numList[j]
+            numList[j] = temp
+        j -= 1
+print(numList)
