d9369cb1d03a24dd98dae508c6f80a2a57f993fb
Adam Baylin <vip.phamquangthinh@gmail.com>
2018-01-15 23:28:25 +0700

    1501

--- Test.py
+++ Test.py
@@ -1,15 +1,11 @@
-import random
-numList = []
-for i in range(100):
-    numList.append(random.randint(1,1000))
+class Bankacc:
+    rate = 0.0
+    @staticmethod
+    def setrate(newrate):
+        Bankacc.rate = newrate
 
-print(numList)
-for i in range(1,len(numList)):
-    j = i
-    while j > 0:
-        if numList[j] < numList[j - 1]:
-            temp = numList[j-1]
-            numList[j-1] = numList[j]
-            numList[j] = temp
-        j -= 1
-print(numList)
+acb = Bankacc()
+Bankacc.setrate(0.1)
+
+print (acb.rate)
+
