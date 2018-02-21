f21160cc7acaee510620e8d82add50f30b936d02
Adam Baylin <vip.phamquangthinh@gmail.com>
2018-02-21 12:35:24 +0700

    level up

--- Test.py
+++ Test.py
@@ -1,10 +1,7 @@
-posts = [
-    {
-        'author': {'username': 'John'},
-        'body': 'Beautiful day in Portland!'
-    },
-    {
-        'author': {'username': 'Susan'},
-        'body': 'The Avengers movie was so cool!'
-    }
-]
+import random
+for i in range (10):
+    name = 'Luxury'
+    char = random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    char2 = random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    code = ('{0}-{1}{2}{3:04}'.format( name,char,char2,i))
+    print (code)
