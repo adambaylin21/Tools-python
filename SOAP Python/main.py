3c72b3d05e043006d36e19d130183bc6b389e317
phamquangthinhs <vip.phamquangthinh@gmail.com>
2018-03-28 16:46:42 +0700

    SOAP Python

--- SOAP Python/main.py
+++ SOAP Python/main.py
@@ -1,16 +1,39 @@
 import requests
 from requests.auth import HTTPBasicAuth
 import base64
+from xml.dom import minidom
+
+
+# [Main Function]
+def sendsms(text,phone):
+    smstext = str.encode(text)
+    messenger = base64.b64encode(smstext)
+    body = (body1 + phone + body2 + messenger.decode("utf-8") + body3)
+    response = requests.post(url,data=str(body),headers=headers,auth=('luxuryfan', 'bxluxur@678#'))
+    with open('output.xml', 'w') as post:
+        post.write((response.content).decode("utf-8"))
+    xmldoc = minidom.parse('output.xml')
+    itemlist = xmldoc.getElementsByTagName("result")[0]
+
+    # Edit to 0 if run in server
+    if getNodeText(itemlist) == '-4':
+        print ('Done') 
+
+
+# Get val Node Text
+def getNodeText(node):
+    nodelist = node.childNodes
+    result = []
+    for node in nodelist:
+        if node.nodeType == node.TEXT_NODE:
+            result.append(node.data)
+    return ''.join(result)
+
 
 url="http://sms.8x77.vn:8077/mt-services/MTService?wsdl"
 headers = {'content-type': 'application/soap+xml'}
 # headers = {'content-type': 'text/xml'}
 
-
-
-
-smstext = b"Hello"
-messenger = base64.b64encode(smstext)
 body1 = """
 <!--REQUEST.................-->
 <env:Envelope  xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/"
@@ -21,10 +44,12 @@ body1 = """
  </env:Header>
  <env:Body   env:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
   <m:sendMT    xmlns:m="MTService">
-   <string     xsi:type="xsd:string">0972937793</string>
+   <string     xsi:type="xsd:string">"""
+
+body2 = """</string>
    <string0     xsi:type="xsd:string">"""
 
-body2 = """</string0>
+body3 = """</string0>
    <string1     xsi:type="xsd:string">LUXURYFAN</string1>
    <string2     xsi:type="xsd:string">LUXURYFAN</string2>
    <string3     xsi:type="xsd:string">0</string3>
@@ -36,9 +61,7 @@ body2 = """</string0>
   </m:sendMT>
  </env:Body>
 </env:Envelope>"""
-body = (body1 + messenger.decode("utf-8") + body2)
-# response = requests.post(url,data=str(body),headers=headers,auth=('luxuryfan', 'bxluxur@678#'))
-# print (response.content)
-# with open('output.xml', 'w') as post:
-#     post.write(str(response.content))
-print (messenger)
+
+
+if __name__ == '__main__':
+    sendsms('Xin Chao','0964133556')
