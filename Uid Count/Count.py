a39021180f383e9720e3c20126c278d217a00cdf
adambaylin <vip.phamquangthinh@gmail.com>
2018-10-25 13:07:47 +0700

    Uid count

---
+++ Uid Count/Count.py
@@ -0,0 +1,46 @@
+import os
+
+def where_mf(where):
+    fileDir = os.path.dirname(os.path.realpath('__file__'))
+    rel_path = where
+    abs_file_path = os.path.join(fileDir, rel_path)
+    return abs_file_path
+
+def get_upload(where):
+    a = os.listdir(where)
+    a.sort(key=lambda f: int(filter(str.isdigit, f)))
+    return a
+
+def open_file(stt,filename):
+    with open(filename, 'r') as fp:
+        line = fp.readline()
+        while line:
+            uid = line.strip()
+            stt += 1
+            line = fp.readline()
+    return stt
+
+def main_count(folder):
+    where = where_mf(folder)
+    upload = get_upload(where)
+    b = len(upload)
+    i = 0
+    stt = 0
+    while i < b:
+        name = upload[i]
+        i += 1
+        file = open_file(stt,'{}{}'.format(folder,name))
+        stt = file
+    return stt
+
+def sub_folder():
+	where = where_mf('')
+	a = [x[1] for x in os.walk(where)]
+	for b in a:
+		for c in b:
+			folder = '{}/'.format(c)
+			d = main_count(folder)
+			print ('Folder: {} - {} UID'.format(c,d))
+
+if __name__ == "__main__":
+	sub_folder()
